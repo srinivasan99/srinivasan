250002b3e12038eb2a398cb5b75797ab447b8055
srinivasan99 <52272972+srinivasan99@users.noreply.github.com>
2019-06-27 23:25:04 -0700

    Create srini3.py

---
+++ srini3.py
@@ -0,0 +1,6 @@
+n9=input()
+c=['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
+if n9 in c:
+   print("Alphabet")
+else:
+   print("No")
