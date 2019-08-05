58c90ee3ef76648508c59bb74abb68cb86c7921b
srinivasan99 <52272972+srinivasan99@users.noreply.github.com>
2019-08-05 03:07:16 -0700

    Update srini131.py

--- srini131.py
+++ srini131.py
@@ -1,9 +1,9 @@
 import sys, string, math
 b = int(input())
-m = n
+m = b
 L = []
 for i in range( 2,m) :
     if b%i == 0 : L.append(i)
-    while n%i == 0 : b //= i
+    while b%i == 0 : b //= i
 if len(L) == 0 : print(m)
 else :           print(*L)
