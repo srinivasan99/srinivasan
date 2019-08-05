809fda30f74eaf9fb82ecc21931793748b6df40b
srinivasan99 <52272972+srinivasan99@users.noreply.github.com>
2019-08-05 01:41:14 -0700

    Create srini117.py

---
+++ srini117.py
@@ -0,0 +1,5 @@
+import sys, string, math
+sri = input()
+L = ['','I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+i = L.index(sri)
+print(i)
