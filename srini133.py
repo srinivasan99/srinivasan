e10ae142b69139cb4ceca02a931a70440e9b0d15
srinivasan99 <52272972+srinivasan99@users.noreply.github.com>
2019-08-05 03:25:31 -0700

    Update srini133.py

--- srini133.py
+++ srini133.py
@@ -1,4 +1,15 @@
 import sys, string, math
-k= input()
-if k == 'Sunday' or k == 'Saturday' : print('yes')
-else :                                print('no')
+
+def collinear(r1, y1, x2, y2,x3, y3):
+    if ((y3 - y2)*(x2 - r1) == (y2 - y1)*(x3 - x2)):
+        return  "yes"
+    else:
+        return "no"
+
+
+r1,y1 = map(int,input().split())
+x2,y2 = map(int,input().split())
+x3,y3 = map(int,input().split())
+res = collinear(r1, y1, x2, y2, x3, y3)
+print(res)
+
