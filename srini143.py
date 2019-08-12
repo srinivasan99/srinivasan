422e57ce671c79e886d76c7fb1c1649c0b46eb89
srinivasan99 <52272972+srinivasan99@users.noreply.github.com>
2019-08-12 14:34:01 +0530

    Update srini143.py

--- srini143.py
+++ srini143.py
@@ -1,2 +1,9 @@
-sri=input().__add__(".")
-print(sri)
+sri=input()
+m=0
+for i in range(len(sri)-1):
+    for j in range(i+1,len(sri)):
+        k=sri[i:j]
+        if k==k[::-1]:
+            if len(k)>m:
+                m=len(k)
+print(m)
